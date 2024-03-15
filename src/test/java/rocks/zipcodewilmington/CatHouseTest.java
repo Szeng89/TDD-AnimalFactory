883c41be5bb6d4f2130021b2291303cc865b2ca3
Szeng89 <shijinz898@gmail.com>
2024-03-15 09:10:39 -0400

    finished CatTest

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,12 +1,60 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
 /**
  * @author leon on 4/19/18.
  */
 public class CatHouseTest {
     // TODO - Create tests for `void add(Cat cat)`
+
+    @Test
+    public void testAddCat() {
+        CatHouse cathouse = new CatHouse();
+        Cat cat = new Cat(null, null ,null);
+        int expectedHowManyCatWeHave = cathouse.getNumberOfCats(); //0
+        cathouse.add(cat); //1
+
+        int actualHowManyCatWeHave = cathouse.getNumberOfCats();
+
+        Assert.assertEquals(expectedHowManyCatWeHave + 1, actualHowManyCatWeHave);
+
+    }
+
     // TODO - Create tests for `void remove(Integer id)`
+
+    @Test
+    public void testRemoveIntegerId() {
+        CatHouse cathouse = new CatHouse();
+        Integer id = 6789;
+        Cat cat = new Cat(null,null,id);
+        cathouse.add(cat);
+        int expectedHowManyCatsWeHave = cathouse.getNumberOfCats();
+
+        cathouse.remove(id);
+
+        int actualHowManyCatsWeHave = cathouse.getNumberOfCats();
+
+        Assert.assertEquals(expectedHowManyCatsWeHave - 1, actualHowManyCatsWeHave);
+    }
     // TODO - Create tests for `void remove(Cat cat)`
+    @Test
+    public void testRemoveCat() {
+        CatHouse cathouse = new CatHouse();
+        Cat cat = new Cat(null,null,null);
+        cathouse.add(cat);
+        int howMany = cathouse.getNumberOfCats();
+
+        cathouse.remove(cat);
+
+        int actualHowMany = cathouse.getNumberOfCats();
+
+        Assert.assertEquals(howMany - 1 ,actualHowMany);
+    }
+
     // TODO - Create tests for `Cat getCatById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfCats()`
 }
--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -2,7 +2,9 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Mammal;
 
 import java.util.Date;
 
@@ -11,17 +13,99 @@ import java.util.Date;
  */
 public class CatTest {
     // TODO - Create tests for `void setName(String name)`
+    @Test
+    public void testSetName(){
+        // Given
+        Cat cat = new Cat(null,null,null);
+        String expected = "jack";
+        cat.setName(expected);
+
+        //
+        String actual = cat.getName();
+
+
+        // then
+        Assert.assertEquals(expected, actual);
+    }
     // TODO - Create tests for `speak`
+    @Test
+    public void testSpeak() {
+        Cat cat = new Cat(null,null, null);
+        String expected = "meow!";
+
+        String actual = cat.speak();
+
+        Assert.assertEquals(expected, actual);
+
+
+    }
     // TODO - Create tests for `setBirthDate(Date birthDate)`
+    @Test
+    public void testSetBirthDate() {
+        // Given
+        Cat cat = new Cat(null, null, null);
+        Date expected = new Date(2011, 06, 27);
+
+        // When
+        cat.setBirthDate(expected);
+        Date actual = cat.getBirthDate();
+
+        Assert.assertEquals(expected, actual);
+    }
     // TODO - Create tests for `void eat(Food food)`
+    @Test
+    public void testEat() {
+        // Given
+        Cat cat = new Cat(null, null, null);
+        Food food = new Food();
+
+        // When
+        int expected = cat.getNumberOfMealsEaten();
+        cat.eat(food);
+        int actual = cat.getNumberOfMealsEaten();
+
+        // Then
+        Assert.assertEquals(expected + 1, actual);
+    }
     // TODO - Create tests for `Integer getId()`
+    @Test
+    public void testGetId() {
+        // Given
+        int expected = 6789;
+        Cat cat = new Cat(null, null, expected);
+
+        // When
+        int actual = cat.getId();
+
+        //then
+        Assert.assertEquals(expected, actual);
+
+
+    }
     // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
+    @Test
+    public void testAnimalInheritance() {
+        Cat cat = new Cat(null, null, null);
+
+
+        Assert.assertTrue(cat instanceof Animal);
+
+
+    }
+
     // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
 
 
     @Test
+    public void testMammalInheritance() {
+        Cat cat = new Cat(null,null,null);
+
+        Assert.assertTrue(cat instanceof Mammal);
+    }
+
+    @Test
     public void constructorTest() {
-        // Given (cat data)
+        // Given (cat data) EXPECTED
         String givenName = "Zula";
         Date givenBirthDate = new Date();
         Integer givenId = 0;
@@ -29,7 +113,7 @@ public class CatTest {
         // When (a cat is constructed)
         Cat cat = new Cat(givenName, givenBirthDate, givenId);
 
-        // When (we retrieve data from the cat)
+        // When (we retrieve data from the cat) ACTUAL
         String retrievedName = cat.getName();
         Date retrievedBirthDate = cat.getBirthDate();
         Integer retrievedId = cat.getId();
